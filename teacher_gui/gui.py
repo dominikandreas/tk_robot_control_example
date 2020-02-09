b77a0f9d0ba7c2a632e54b6480be438f3dc586d3
dominik andreas <admin@localhost>
2020-02-09 11:04:52 +0100

    revert some changes

--- teacher_gui/gui.py
+++ teacher_gui/gui.py
@@ -1,29 +1,16 @@
 from tkinter import Tk, Frame, Button, Label, Text, Spinbox, END
 from teacher_gui.robot import Robot
-from teacher_gui.lang import Translations as T
 
 
-class JointControlButton(Button):
-    def __init__(self, master, text, step_size, callback):
-        super().__init__(master, text=text, command=self.move)
+class JointControlButton:
+    def __init__(self, master, text, step_size, callback, column, row):
         self.step_size = step_size
         self.callback = callback
+        self.btn = Button(master, text=text, command=self.move)
+        self.btn.grid(column=column, row=row)
 
     def move(self):
         self.callback(self.step_size)
-
-
-class ManualControl(Frame):
-    def __init__(self, master, label, min_step, max_step, control_callback, **options):
-        super().__init__(master, **options)
-        self.spin_box_steps = Spinbox(self, from_=min_step, to=max_step)  # Anzahl der Schritte eingeben
-        self.spin_box_steps.grid(column=0, row=0)  # Spin Box Position festlegen
-
-        self.button_accept_position = Button(self, text=label,
-                                             command=control_callback).grid(column=1, row=0)
-
-    def get_value(self):
-        return int(self.spin_box_steps.get())
 
 
 class JointControl(Frame):
@@ -32,23 +19,24 @@ class JointControl(Frame):
         self.name = name
         self.index = index
         self.robot = robot
-        self.label_name = Label(master, text=name + ": ").grid(column=0, row=0)
+        self.label_name = Label(master, text=name + ": ").grid(column=0, row=index)
 
-        self.manual_control = ManualControl(master, label=name + T.accept_pos(),
-                                            min_step=min_step, max_step=max_step,
-                                            control_callback=self.accept_position)
-        self.manual_control.grid(column=1, row=index)
+        self.spin_box_steps = Spinbox(master, from_=min_step, to=max_step)  # Anzahl der Schritte eingeben
+        self.spin_box_steps.grid(column=1, row=index)  # Spin Box Position festlegen
 
-        self.label_current_pos = Label(master, text="%s %s:" % (name, T.current_pos())).grid(column=2, row=0)
+        self.button_accept_position = Button(master, text=name + " Position uebernehmen",
+                                             command=self.accept_position)
+        self.button_accept_position.grid(column=1, row=index)
+
+        self.label_current_pos = Label(master, text=name + " Aktuelle Position: ").grid(column=3, row=index)
         self.text_pos = Text(master, height=1, width=20)
-        self.text_pos.grid(column=3, row=0)
-        self.update_text(self.robot.current_state[self.index])
+        self.text_pos.grid(column=4, row=index)
+        self.text_pos.insert("1.0", self.robot.current_state[self.index])
 
         self.step_buttons = []
         for list_index, step in enumerate([-step_size, step_size, -step_size*5, step_size*5]):
-            text = T.current_pos() + " " + ("" if step < 0 else "+") + str(step)
-            button = JointControlButton(master, text, step, callback=self.move)
-            button.grid(column=4 + list_index, row=index)
+            text = "Aktuelle Position " + ("" if step < 0 else "+") + str(step)
+            button = JointControlButton(master, text, step, column=5 + list_index, row=index, callback=self.move)
             self.step_buttons.append(button)
 
     def update_text(self, content):
@@ -61,15 +49,15 @@ class JointControl(Frame):
         self.update_text(self.robot.current_state[self.index])
 
     def accept_position(self):
-        steps = self.manual_control.get_value()
-        print(self.name + T.steps() + ": " + str(steps))
+        steps = int(self.spin_box_steps.get())
+        print(self.name + "Schritte: " + str(steps))
         self.robot.set_joint(self.index, steps)
 
 
 class RobotControlGui(Tk):
     def __init__(self, robot):
         super().__init__()
-        self.title("Position Teacher")
+        self.title("Position Teacher app")
         self.geometry('1500x1500')
         self.robot = robot
         self.joint_controls = []
